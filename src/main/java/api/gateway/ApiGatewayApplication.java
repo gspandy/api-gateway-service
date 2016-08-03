56bb4b38a3b98a80dcb6e0ae9c06d32d5a7104b1
zhouwei <771287929@qq.com>
2016-08-03 17:44:42 +0800

    init project

---
+++ src/main/java/api/gateway/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package api.gateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
+
+
+@SpringBootApplication
+@EnableSidecar
+public class ApiGatewayApplication {
+    public static void main( String[] args ) {
+        new SpringApplicationBuilder(ApiGatewayApplication.class).web(true).run(args);
+    }
+    
+}
