2c7c6bb6d67d8e64dc930a27181addffe7e5a6de
songzhiqiang <yuanfenskyip@163.com>
2016-08-17 15:33:10 +0800

    修改url

--- src/main/resources/static/js/commonUrl.js
+++ src/main/resources/static/js/commonUrl.js
@@ -12,9 +12,9 @@
 	 **/
 	
 const map = new Map();
-map.set(0,"http://192.168.2.114:10001/viewMS/view/mList");
+map.set(0,"http://192.168.0.133:10001/viewMS/view/mList");
 map.set(1,"mock/tree.json");
-map.set(2,"http://192.168.2.114:10001/viewMS/view/mList");
+map.set(2,"http://192.168.0.133:10001/viewMS/view/mList");
 const mapchild = new Map();
 mapchild.set(0,"mock/trip.json");
 mapchild.set(1,"mock/showHomework.json");
